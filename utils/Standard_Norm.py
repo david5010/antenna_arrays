87b18db777c0ae0728436a0e16097c06ff3a6505
David Lu <davidlu5010@gmail.com>
2023-06-05 20:47:06 -0700

    newly trained transformer

--- utils/Standard_Norm.py
+++ utils/Standard_Norm.py
@@ -39,48 +39,64 @@ class ScaleCost:
         self.mean = scaler_attributes['mean']
         self.scale = scaler_attributes['scale']
 
-class StandardNorm:
+class ScaleYZCost:
     """
-    Standard Normalizer -> Transforms
+    Scale All
     """
-    def __init__(self, mean=None, std=None):
-        self.mean = mean
-        self.scale = std
+    def __init__(self, y_mean = None, z_mean = None,
+                 y_scale = None, z_scale = None,
+                 cost_mean = None, cost_scale = None) -> None:
+        self.y_mean = y_mean
+        self.z_mean = z_mean
+        self.y_scale = y_scale
+        self.z_scale = z_scale
+        self.cost_mean = cost_mean
+        self.cost_scale = cost_scale
 
-    def fit(self, data):
-        num_columns = data.size(1)
-        self.mean = torch.zeros(num_columns)
-        self.scale = torch.zeros(num_columns)
+    def fit(self, y_data, z_data, cost_data):
+        """
+        Don't count the 0
+        """
+        y_mask = (y_data != 0)
+        non_padded_y = y_data[y_mask]
+        self.y_mean = non_padded_y.mean()
+        self.y_scale = non_padded_y.std()
+
+        z_mask = (z_data != 0)
+        non_padded_z = z_data[z_mask]
+        self.z_mean = non_padded_z.mean()
+        self.z_scale = non_padded_z.std()
+
+        self.cost_mean = np.mean(cost_data)
+        self.cost_scale = np.std(cost_data)
+    
+    def transform(self, y_data, z_data, cost_data):
         
-        for column_idx in range(num_columns):
-            column = data[:, column_idx]
-            non_zero_values = column[column != 0]  # Filter out padding zeros
-            
-            if non_zero_values.numel() > 0:  # Check if there are non-zero values
-                mean = torch.mean(non_zero_values)
-                std = torch.std(non_zero_values)
-                
-                if math.isnan(mean):
-                    mean = 0
-                if math.isnan(std):
-                    std = 1
-            else:  # All values in the column are zeros
-                mean = 0
-                std = 1
-                
-            self.mean[column_idx] = mean
-            self.scale[column_idx] = std
-    
-    def transform(self, data):
-        if self.mean is None or self.scale is None:
-            raise ValueError("Scaler has not been fitted")
-        return (data - self.mean) / self.scale
+        scaled_y = (y_data - self.y_mean)/self.y_scale
+        scaled_z = (z_data - self.z_mean)/self.z_scale
+        scaled_cost = (cost_data - self.cost_mean)/self.cost_scale
+        scaled_cost = scaled_cost.reshape(-1,1)
+        combined = np.concatenate((scaled_y, scaled_z, scaled_cost), axis = 1)
+        return combined
 
-    def inverse_transform(self, scaled_data):
-        if self.mean is None or self.scale is None:
-            raise ValueError("Scaler has not been fitted")
-        return (scaled_data * self.scale) + self.mean
+    def inverse_transform(self, scaled_y, scaled_z, scaled_cost):
+        y = (scaled_y * self.y_scale) + self.y_mean
+        z = (scaled_z * self.z_scale) + self.z_mean
+        cost = (scaled_cost * self.cost_scale) + self.cost_mean
+
+        return np.concatenate((y,z,cost), axis = 1)
     
+    def get_stats(self):
+        stats = {
+            'y_mean': self.y_mean,
+            'z_mean': self.z_mean,
+            'cost_mean': self.cost_mean,
+            'y_scale': self.y_scale,
+            'z_scale': self.z_scale,
+            'cost_scale': self.cost_scale
+        }
+        return stats
+
     def save_scaler(self, path):
         """
         Saves the fitted parameters
@@ -95,3 +111,4 @@ class StandardNorm:
         scaler_attributes = torch.load(path)
         self.mean = scaler_attributes['mean']
         self.scale = scaler_attributes['scale']
+
